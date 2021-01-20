a1de18aaebbdd841933336b19d1c92dd6e979b5f
iremSa <saribiyikirem95@gmail.com>
2021-01-20 19:47:24 +0300

    first commit

---
+++ MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi:" + musteri.Adi +" "+ musteri.Soyadi);
+            Console.WriteLine("Müşteri Yaşı:" + musteri.Yasi );
+            Console.WriteLine("Müşteri Telefon Numarası:" + musteri.Telefonumarasi);
+
+
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi:" + musteri.Adi+ " "  + musteri.Soyadi);
+            
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listelendi:" + musteri.Adi + " "+ musteri.Soyadi);
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "İrem";
+            musteri1.Soyadi = "Sarıbıyık";
+            musteri1.Yasi = 24;
+            musteri1.Telefonumarasi = 05411232456;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ayşe";
+            musteri2.Soyadi = "Yılmaz";
+            musteri2.Yasi = 26;
+            musteri2.Telefonumarasi = 05411232855;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Yasemin";
+            musteri3.Soyadi = "Küçükyalı";
+            musteri3.Yasi = 30;
+            musteri3.Telefonumarasi = 05411246001;
+
+            Musteri[] musteriler = new Musteri[]{ musteri1, musteri2, musteri3 };
+
+            MusteriManager musterimanager = new MusteriManager();
+            Console.WriteLine("Eklenen  müşteriler:");
+
+            musterimanager.Ekle(musteri1);
+            musterimanager.Ekle(musteri2);
+            musterimanager.Ekle(musteri3);
+            Console.WriteLine(" ");
+            Console.WriteLine("Listelenen müşteriler:");
+            Console.WriteLine(" ");
+
+            MusteriManager musterimanager2 = new MusteriManager();
+            musterimanager.Listele(musteri1);
+            musterimanager.Listele(musteri2);
+            musterimanager.Listele(musteri3);
+            Console.WriteLine(" ");
+            Console.WriteLine("Silinen müşteriler:");
+            Console.WriteLine(" ");
+
+            MusteriManager musterimanager3 = new MusteriManager();
+            musterimanager.Sil(musteri1);
+            musterimanager.Sil(musteri2);
+            musterimanager.Sil(musteri3);
+
+
+
+
+        }
+
+    }
+}
